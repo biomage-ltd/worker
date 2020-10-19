0d748db2eaadb53d7c6f0e530893a0a62b2bd476
Marcell Pek <marcell.pek@marcellp.net>
2020-10-19 16:03:56 +0100

    fix

--- r/src/work.r
+++ r/src/work.r
@@ -1,6 +1,22 @@
 library(RestRserve)
+library(pagoda2)
+
+message("Launching R worker...")
 
 app <- Application$new()
+
+message("Loading R object....")
+
+data <- readRDS("/data/r.rds")
+
+message("Initializing handlers...")
+
+app$add_post(
+    path = "/DifferentialExpression",
+    FUN = function(request, response) {
+        response$set_body("up")
+    }
+)
 
 app$add_get(
     path = "/health",
@@ -8,6 +24,9 @@ app$add_get(
         response$set_body("up")
     }
 )
+
+message("Starting....")
+
 
 backend <- BackendRserve$new()
 backend$start(app, http_port = 4000)
